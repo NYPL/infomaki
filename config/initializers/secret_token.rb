d2e9dfef055d5b1117be162ae2cbae1bdb17ba7d
Clint Newsom <hcnewsom@gmail.com>
2012-08-27 15:20:45 -0400

    New Rails 3 files. Beginning of app conversion

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Infomaki::Application.config.secret_token = '24d6c69e143589288adc6d5a3a20c5dcfc435dd47aa2ef4abc06a4b17d54f20d4910771cc38dea535238499a767464b55262e5b58168609d31a78c8e5c2e3487'
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,70 @@
-ActionController::Routing::Routes.draw do |map|
+Infomaki::Application.routes.draw do
+  # The priority is based upon order of creation:
+  # first created -> highest priority.
 
-  map.resources :answers, :card_sorts, :quizzes, :pageviews, :replies, :organizations, :initiatives, 
+  # Sample of regular route:
+  #   match 'products/:id' => 'catalog#view'
+  # Keep in mind you can assign values other than :controller and :action
+
+  # Sample of named route:
+  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
+  # This route can be invoked with purchase_url(:id => product.id)
+
+  # Sample resource route (maps HTTP verbs to controller actions automatically):
+  #   resources :products
+
+  resources :answers, :card_sorts, :quizzes, :pageviews, :replies, :organizations, :initiatives, 
     :wireframes, :card_groups, :cards, :users
 
-  map.root :controller => "about"
+  root :controller => "about"
 
-  map.connect 'participate', :controller => 'initiatives', :action => 'random'
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
+  #map.connect 'participate', :controller => 'initiatives', :action => 'random'
+  match 'participate' => 'initiatives#random'
+  #map.connect ':controller/:action/:id'
+  match ':controller/:action/:id'
+  #map.connect ':controller/:action/:id.:format'
+  match ':controller/:action/:id.:format'
 
+  # Sample resource route with options:
+  #   resources :products do
+  #     member do
+  #       get 'short'
+  #       post 'toggle'
+  #     end
+  #
+  #     collection do
+  #       get 'sold'
+  #     end
+  #   end
+
+  # Sample resource route with sub-resources:
+  #   resources :products do
+  #     resources :comments, :sales
+  #     resource :seller
+  #   end
+
+  # Sample resource route with more complex sub-resources
+  #   resources :products do
+  #     resources :comments
+  #     resources :sales do
+  #       get 'recent', :on => :collection
+  #     end
+  #   end
+
+  # Sample resource route within a namespace:
+  #   namespace :admin do
+  #     # Directs /admin/products/* to Admin::ProductsController
+  #     # (app/controllers/admin/products_controller.rb)
+  #     resources :products
+  #   end
+
+  # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+  # root :to => 'welcome#index'
+
+  # See how all your routes lay out with "rake routes"
+
+  # This is a legacy wild controller route that's not recommended for RESTful applications.
+  # Note: This route will make all actions in every controller accessible via GET requests.
+  # match ':controller(/:action(/:id))(.:format)'
 end
