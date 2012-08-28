b1291f7b5fba7b11c82f95e8e90224dd4db34561
Clint Newsom <hcnewsom@gmail.com>
2012-08-28 07:57:00 -0400

    rails3ifying migrations

--- db/migrate/20090127195917_add_fields_to_people.rb
+++ db/migrate/20090127195917_add_fields_to_people.rb
@@ -1,10 +1,13 @@
 class AddFieldsToPeople < ActiveRecord::Migration
   def self.up
+    attr_accessible :name
+
     create_table :organizations_testees do |t|
       t.integer :organization_id
       t.integer :testee_id
     end
-    Organization.create!(:name => "New York Public Library")
+    Organization.create(:name => "New York Public Library")
+
   end
 
   def self.down
