df3cbd82232a3039cdd8c3bbaeb3ed47af5a118b
Clint Newsom <hcnewsom@gmail.com>
2012-08-28 07:56:14 -0400

    adding session info from legacy app

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,18 @@
 # Be sure to restart your server when you modify this file.
 
-Infomaki::Application.config.session_store :cookie_store, key: '_infomaki_session'
+Infomaki::Application.config.session_store :cookie_store, key: '_infomaki_session', secret: '3477777263746733823748777abbc767a6c7676a77766a67ddddfefefef87877a87a8a7875322cccc'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
 # Infomaki::Application.config.session_store :active_record_store
+
+
+
+
+
+
+
+
+
+
